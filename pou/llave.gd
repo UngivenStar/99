13bfc59d5ba8eed2bca07a3ed50468ed8bf2b9bd
Ungivenstar <jackynight201@gmail.com>
2025-06-26 09:31:47 -0600

    V2 Puerta + Llave -Carolina

---
+++ pou/llave.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+signal puerta_llave
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.get_name() == "personaje2":
+		emit_signal("puerta_llave")
+		queue_free()
---
+++ pou/puerta.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var llave = 0
+
+
+@onready var ui_label = $"../Puerta/Label"
+
+func _ready() -> void:
+	upgrade_ui()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.get_name() == "personaje2" and llave == 1:
+			get_tree().change_scene_to_file("res://nivel2.1.tscn")
+			
+	elif body.get_name() == "personaje2" and llave == 0:
+		ui_label.text = "Necesitas la llave para abrir la puerta"
+
+
+func _on_llave_puerta_llave() -> void:
+	llave = 1  
+	upgrade_ui()
+
+	
+func upgrade_ui() -> void:
+	if llave == 1:
+		ui_label.text = "Llaves: 1"
+		await get_tree().create_timer(0.5).timeout
+		
+
+	else:
+		ui_label.text = "Llaves: 0"
